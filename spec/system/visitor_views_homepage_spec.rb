ca114974941550dd7f47f53a657bf7ecc770109c
sabinopa <priasabino@gmail.com>
2024-03-18 10:58:59 -0300

    build: visitante vê tela inicial da aplicacao com nome

---
+++ spec/system/visitor_views_homepage_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'Visitor views homepage' do
+    it 'and sees the apps name' do
+        visit '/'
+        expect(page).to have_content 'Delivery-APP'
+    end
+end
