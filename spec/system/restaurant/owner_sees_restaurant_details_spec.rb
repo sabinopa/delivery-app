bca74c0c9d8797e55e9bedf65722871ad7b52f1d
sabinopa <priasabino@gmail.com>
2024-03-22 16:37:02 -0300

    dono vê detalhes de seu restaurante e retorna para home page

--- spec/system/restaurant/owner_sees_restaurant_details_spec.rb
+++ spec/system/restaurant/owner_sees_restaurant_details_spec.rb
@@ -33,4 +33,28 @@ describe 'Owner sees restaurant details' do
     expect(page).to have_content 'Não possui opções sem glúten.'
     expect(page).to have_content 'Pagamentos por: PIX | Cartão de Crédito | Cartão de Débito'
   end
+
+  it 'and returns to home page' do
+    pix = PaymentMethod.create!(method: 'PIX')
+    credito = PaymentMethod.create!(method: 'Cartão de Crédito')
+    debito = PaymentMethod.create!(method: 'Cartão de Débito')
+    owner = Owner.create!(email: 'priscila@email.com', password: '12345678')
+    restaurante = Restaurant.create!(owner: owner, brand_name: 'Cantina Mediterrânea', corporate_name: 'Sabores do Mar Mediterrâneo Ltda',
+                                    registration_number: '98.765.432/0001-11', phone_number: '(11) 99876-5432',
+                                    email: 'contato@cantinamediterranea.com.br', address: 'Rua das Oliveiras, 5678',
+                                    neighborhood: 'Bairro Italiano', city: 'Rio de Janeiro', state: 'RJ', zipcode: '21000-000',
+                                    description: 'A Cantina Mediterrânea traz os mais frescos ingredientes do mar para a sua mesa,
+                                    com pratos inspirados na rica culinária mediterrânea.', cancelation_policy: 'Cancelamentos podem
+                                    ser feitos até 48 horas antes da reserva.', estimated_time: 50, vegan_options: true,
+                                    vegetarian_options: true, gluten_free_options: false)
+                                    restaurante.payment_methods << [pix, credito, debito]
+    login_as(owner, :scope => :owner)
+    visit root_path
+    within 'nav' do
+      click_on 'Meu Restaurante'
+    end
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
+  end
 end
